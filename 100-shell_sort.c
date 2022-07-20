74ae19bde36d3bbb2c98f1ab660d97b31e8301b8
Victor <victorblessed9@gmail.com>
2022-07-20 16:23:24 +0100

    new

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,57 +1,46 @@
 #include "sort.h"
-
 /**
- * shell_sort - sorting algorithm
- * @array: array to sort
- * @size: size of array
- * Return: void
+ * shell_sort - function that sorts an array
+ * of integers in ascending order using the
+ * Shell sort algorithm
+ *
+ * @array: input arrray
+ * @size: size of the array
+ * Return: no return
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t jump, i, n, a;
+	size_t n = 1;
 
-	for (jump = 1; jump < size; jump = 3 * jump + 1)
-		;
-	for (jump = jump / 3; jump > 0; jump = jump / 3)
+	while (n < size)
+		n = (n * 3) + 1;
+
+	while ((n = (n - 1) / 3) > 0)
+		_ssort(array, size, n);
+}
+/**
+ * _ssort - auxiliar function for
+ * shell_sort function
+ * @a: input arrray
+ * @size: size of the array
+ * @n: intervale
+ * Return: no return
+ */
+void _ssort(int *a, int size, int n)
+{
+	int tmp, i, j;
+
+	for (i = 0; (i + n) < size; i++)
 	{
-		for (i = 0; i < jump; i++)
+		for (j = i + n; (j - n) >= 0; j = j - n)
 		{
-			n = i;
-			while (n < size - jump)
+			if (a[j] < a[j - n])
 			{
-				if (array[n] > array[n + jump])
-				{
-					swap(&array[n],
-					     &array[n + jump]);
-					for (a = n; a >= jump; a = a - jump)
-					{
-					if (array[a] < array[a - jump])
-					{
-					swap(&array[a],
-					&array[a - jump]);
-					}
-					else
-					break;
-					}
-				}
-				else
-					n = n + jump;
+				tmp = a[j];
+				a[j] = a[j - n];
+				a[j - n] = tmp;
 			}
 		}
-		print_array(array, size);
 	}
-}
-
-/**
- * swap - function to swap elements
- * @a: 1st element
- * @b: 2nd element
- */
-void swap(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	print_array(a, size);
 }
