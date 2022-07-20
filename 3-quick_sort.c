2e25426369c143c5bd4082cdb9f4c51e7a29f5ac
Victor <victorblessed9@gmail.com>
2022-07-20 16:20:22 +0100

    new

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,89 +1,57 @@
 #include "sort.h"
-
 /**
- * quick_sort - function to quick sort an array using lomuto method
- * @array: the array to be sorted
+ * quick_sort - function that sorts an array
+ * of integers in ascending order using the
+ * quick sort algorithm
+ *
+ * @array: input arrray
  * @size: size of the array
- * Return: void
+ * Return: no return
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size == 0 || size == 1)
-		return;
-	sort(array, 0, size - 1, size);
+	_qsort(array, 0, size - 1, size);
 }
-
 /**
- * sort - function to call recursive and create new pivots
- * @array: array to be sorted
- * @start: beginning of sorting array
- * @end: end of sorting array
- * @size: size of array
- * Return: void
+ * _qsort - auxiliar function for the
+ * quick_sort function
+ * @a: input arrray
+ * @low: index for the first element
+ * @high: index for the last element
+ * @size: size of the array
+ * Return: no return
  */
-void sort(int *array, size_t start, size_t end, size_t size)
+void _qsort(int *a, int low, int high, int size)
 {
-	size_t pivot;
+	int p, w, i;
+	int tmp;
 
-	if (end == 0 || end <= start)
-		return;
-	pivot = partition(array, start, end, size);
-	if (pivot != 0 && pivot > start)
-		sort(array, start, pivot - 1, size);
-	if (pivot < size - 1)
-		sort(array, pivot + 1, end, size);
-}
-
-/**
- * partition - function to partition array according to pivot
- * @array: array to be partitioned
- * @start: beginning of comparison index
- * @pivot: partition index
- * @size: size of array
- * Return: void
- */
-size_t partition(int *array, size_t start, size_t pivot, size_t size)
-{
-	size_t a;
-
-	for (a = start; a < pivot; a++)
+	if (low < high)
 	{
-		if (array[a] < array[pivot])
+		p = high;
+		w = low;
+		for (i = low; i < high; i++)
 		{
-			if (a != start)
+			if (a[i] < a[p])
 			{
-				swap(&array[start], &array[a]);
-/*				temp = array[start]; */
-/*				array[start] = array[a];*/
-/*				array[a] = temp; */
-				print_array(array, size);
+				if (i != w)
+				{
+					tmp = a[i];
+					a[i] = a[w];
+					a[w] = tmp;
+					print_array(a, size);
+				}
+				w++;
 			}
-			start++;
 		}
+		if (w != p && a[w] != a[p])
+		{
+			tmp = a[w];
+			a[w] = a[p];
+			a[p] = tmp;
+			print_array(a, size);
+		}
+		_qsort(a, low, w - 1, size);
+		_qsort(a, w + 1, high, size);
 	}
-	if (array[start] > array[pivot])
-	{
-		swap(&array[start], &array[pivot]);
-/*		temp = array[start];*/
-/*		array[start] = array[pivot];*/
-/*		array[pivot] = temp; */
-		pivot = start;
-		print_array(array, size);
-	}
-	return (pivot);
-}
-
-/**
- * swap - function to swap elements
- * @a: element 1
- * @b: element 2
- * Return: void
- */
-void swap(int *a, int *b)
-{
-	int temp = 0;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
 }
